97aab2f8370accee7e2922b820121bcde2a08368
kunal171 <kunalkunal63@gmail.com>
2024-08-11 18:38:04 +0530

    problems with synchronous model

--- concurency/src/main.rs
+++ concurency/src/main.rs
@@ -1,8 +1,23 @@
-#[tokio::main]
-async fn main() {
-    asynchronous_function().await;
+use std::fs::File;
+use std::io::{self, Read};
+
+fn read_file_sync(path: &str) -> io::Result<String> {
+    let mut file = File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
 }
 
-async fn asynchronous_function() {
-    println!("Hello, async world!");
+fn main() {
+    println!("Task 1 started");
+    std::thread::sleep(std::time::Duration::from_secs(2));
+    println!("Task 1 completed");
+
+    println!("Task 2 started");
+    std::thread::sleep(std::time::Duration::from_secs(2));
+    println!("Task 2 completed");
 }
+
+//In this program, 
+// Task 2 will only start after Task 1 is completed, 
+// resulting in a total wait time of 4 seconds.
