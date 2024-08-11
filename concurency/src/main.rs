2a57d4787b13952f47e99044540ba8e9275b63be
kunal171 <kunalkunal63@gmail.com>
2024-08-11 18:39:06 +0530

    problems with synchronous model

--- concurency/src/main.rs
+++ concurency/src/main.rs
@@ -8,7 +8,7 @@ fn read_file_sync(path: &str) -> io::Result<String> {
     Ok(contents)
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     println!("Task 1 started");
     std::thread::sleep(std::time::Duration::from_secs(2));
     println!("Task 1 completed");
@@ -16,8 +16,14 @@ fn main() {
     println!("Task 2 started");
     std::thread::sleep(std::time::Duration::from_secs(2));
     println!("Task 2 completed");
+
+    let content = read_file_sync("example.txt")?;
+    println!("File content: {}", content);
+    Ok(())
 }
 
 //In this program, 
 // Task 2 will only start after Task 1 is completed, 
 // resulting in a total wait time of 4 seconds.
+// the program will wait for the file to be fully read before printing its content.
+//  If the file reading takes a long time, the entire program will be blocked.
