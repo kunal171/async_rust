e0b68cbd937852db6654036b8b3a0e35ddf93bbc
kunal171 <kunalkunal63@gmail.com>
2024-08-12 17:36:27 +0530

    basics of async await in rust

--- async_await/src/main.rs
+++ async_await/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let result = my_async_function().await;
+    println!("The result is {}", result);
+
+    //Chaining
+    let first = first_async().await;
+    let second = second_async(first).await;
+    println!("Final Result: {}", second);
+    
+
+}
+
+async fn my_async_function() -> i32 {
+    42
+}
+
+//Chaining Async Opereations
+
+async fn first_async() -> i32 {
+    my_async_function().await
+}
+
+async fn second_async(num: i32) -> i32 {
+    num * 2
 }
