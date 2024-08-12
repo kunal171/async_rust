c7633fdc2055ba312c5cf3bd8c47efc4dcf082c3
kunal171 <kunalkunal63@gmail.com>
2024-08-12 20:40:39 +0530

    concurrent task

--- async_await/src/main.rs
+++ async_await/src/main.rs
@@ -1,3 +1,5 @@
+use tokio::time::{sleep, Duration};
+
 #[tokio::main]
 async fn main() {
     let result = my_async_function().await;
@@ -14,7 +16,12 @@ async fn main() {
         Err(err) => println!("Error: {}", err)
     }
     
+    // Concurrent tasks
+    let t1 = task_one();
+    let t2 = task_two();
+    tokio::join!(t1, t2);
 
+    println!("Both Tasks completed");
 }
 
 async fn my_async_function() -> i32 {
@@ -40,4 +47,17 @@ async fn might_fail(flag: bool) -> Result<i32, &'static str> {
     }else {
         Err("Something in the way")
     }
+}
+
+
+// Building Concurrent tasks
+
+async fn task_one() {
+    sleep(Duration::from_secs(3)).await;
+    println!("Task One Completed")
+}
+
+async fn task_two() {
+    sleep(Duration::from_secs(6)).await;
+    println!("Task two Completed")
 }
