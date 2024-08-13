2e17574537d0587025d9db82cbd1fb9099346549
kunal171 <kunalkunal63@gmail.com>
2024-08-13 21:12:06 +0530

    std executor

--- executor/src/main.rs
+++ executor/src/main.rs
@@ -1,3 +1,21 @@
+use async_std::task;
+use futures::join;
+use std::time::Duration;
+
+// A simple async function that simulates some work
+
+async fn async_task(name: &str, duration: u64) {
+    println!("Task {} started", name);
+    task::sleep(Duration::from_secs(duration)).await;
+    println!("Task {} completed", name);
+}
+
 fn main() {
-    println!("Hello, world!");
+    task::block_on(async {
+        let task1 = async_task("one", 2);
+        let task2 = async_task("Two", 3);
+
+        //Run Both Tasks COncurrently
+        join!(task1, task2);
+    });
 }
