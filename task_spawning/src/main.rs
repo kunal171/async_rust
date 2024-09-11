2f873a5fcd4ddde53b7693e8773785d0002e0aa2
kunalUnit <kunal@unitcode.org>
2024-09-11 22:45:45 +0530

    task spawning done

--- task_spawning/src/main.rs
+++ task_spawning/src/main.rs
@@ -1,3 +1,16 @@
+use async_std::task;
+use std::time::Duration;
+
+async fn perform_task() {
+    println!("Task Started");
+    task::sleep(Duration::from_secs(2)).await;
+    println!("Task Completed");
+}
+
 fn main() {
-    println!("Hello, world!");
+    task::block_on(async {
+        task::spawn(perform_task());
+        println!("task spawned");
+        task::sleep(Duration::from_secs(3)).await;
+    });
 }
